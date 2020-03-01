c1850bbbd7a8f27f6d11f4d028445f5d839618d4
Michael Moura <mikemorcerf@gmail.com>
2020-02-29 21:39:52 -0500

    fix crash on index page when there is no coures in db

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,11 +2,17 @@ class CoursesController < ApplicationController
 	#Ignore default action from application controller
 	skip_before_action :require_user
 
+	helper_method :has_courses?
+
 	def index
 		@courses = Course.all
 	end
 
 	def new
 		
+	end
+
+	def has_courses?
+		return Course.all.count>0
 	end
 end
